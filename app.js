325aa652b21425f0f70c3ad6b70a32b501a2064a
Gaurav Chaudhary <gauravchaudhary3000@gmail.com>
2021-07-05 00:48:29 +0530

    Added TITLE Feature in Notes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ showNotes();
 const addBtn = document.getElementById("addBtn");
 addBtn.addEventListener("click", (e) => {
   const addTxt = document.getElementById("addTxt");
+  const addTitle = document.getElementById("addTitle");
   const notes = localStorage.getItem("notes");
   //   console.log(addTxt);
   //   console.log(notes);
@@ -16,9 +17,14 @@ addBtn.addEventListener("click", (e) => {
   } else {
     notesObj = JSON.parse(notes);
   }
-  notesObj.push(addTxt.value);
+  let myObj = {
+    title: addTitle.value,
+    text: addTxt.value,
+  };
+  notesObj.push(myObj);
   localStorage.setItem("notes", JSON.stringify(notesObj));
   addTxt.value = "";
+  addTitle.value = "";
   // console.log(notesObj);
   showNotes();
 });
@@ -36,8 +42,8 @@ function showNotes() {
     html += `
             <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
                     <div class="card-body">
-                        <h5 class="card-title">Note ${index + 1}</h5>
-                        <p class="card-text"> ${element}</p>
+                        <h5 class="card-title">${element.title}</h5>
+                        <p class="card-text"> ${element.text}</p>
                         <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
                     </div>
                 </div>`;
